f3156e2a06ae21ddc886c6acc6b3fce547e1521f
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-08-24 13:45:34 -0700

    add rest of methods, standardize pawn label

--- Source/AmbrosiaLongevity/HediffComp_Longevity.cs
+++ Source/AmbrosiaLongevity/HediffComp_Longevity.cs
@@ -49,12 +49,47 @@ namespace AmbrosiaLongevity
 
         public void Update()
         {
-            //TODO: make withdrawal age pawn (~10yr from beginning to end of normal withdrawal)
             Hediff tolerance = base.Pawn.health.hediffSet.GetFirstHediffOfDef(Props.toleranceDef);
             ToleranceDivisor = Mathf.Clamp(Props.toleranceEffectCurve.Evaluate(tolerance?.Severity ?? 0f), Props.minToleranceFactor, Props.maxToleranceFactor);
             AgeChangePerInterval = Props.severityEffectCurve.Evaluate(base.parent.Severity) / ToleranceDivisor; 
             Pawn_AgeTracker at = base.Pawn.ageTracker;
             TargetAge = AgeFloor + ((at.AgeChronologicalYearsFloat - at.AgeBiologicalYearsFloat) % (AgeCeiling - AgeFloor));
+        }
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            ticksTo--;
+            if (ticksTo <= 0)
+            {
+                //TODO: subtract partial year amounts using floats so I don't need all this <= 0 ? 1 business
+                int ageY = base.Pawn.ageTracker.AgeBiologicalYears;
+                if (ageY < targetAge) base.Pawn.ageTracker.AgeBiologicalTicks += GenDate.TicksPerYear;
+                else if (ageY > targetAge) base.Pawn.ageTracker.AgeBiologicalTicks -= GenDate.TicksPerYear;
+                if (ageY < ageCeiling) RemoveRandOldAgeHediff();
+                //TODO: Facial Stuff compat (make hair less gray, remove wrinkles); assign random non-gray hair colors for vanilla?
+                //wrt above PawnHairColors.RandomHairColor(pawn.story.SkinColor, pawn.ageTracker.AgeBiologicalYears);
+                Reset();
+            }
+        }
+
+        public void RemoveRandOldAgeHediff()
+        {
+            if (base.Pawn.health.hediffSet.hediffs.Where(IsOldAgeRelated).TryRandomElement(out Hediff hediff))
+            {
+                if (hediff != null)
+                {
+                    hediff.Severity = 0f;
+                    if (PawnUtility.ShouldSendNotificationAbout(base.Pawn))
+                    {
+                        Messages.Message("MessageOldAgeDiseaseHealed".Translate(base.parent.LabelCap, base.Pawn.Possessive(), base.Pawn.Named("PAWN"), hediff.Label), base.Pawn, MessageTypeDefOf.PositiveEvent);
+                    }
+                }
+            }
+        }
+
+        private bool IsOldAgeRelated(Hediff h)
+        {
+            return h.def.HasModExtension<AmbrosiaRemoves>();
         }
     }
     class HediffCompProperties_Longevity : HediffCompProperties
